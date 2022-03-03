0e01d36da6726ee5061cf57bae6bc1903b020613
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-03 19:46:37 -0300

    Remove unused function

--- src/guardian/guardian.service.ts
+++ src/guardian/guardian.service.ts
@@ -5,14 +5,6 @@ import { lastValueFrom, map } from 'rxjs';
 @Injectable()
 export class GuardianService {
   constructor(private httpService: HttpService) {}
-  guardianPopularNews() {
-    const guardianApiKey = '6df96c84-1a3f-4b41-b9eb-22e4300af047';
-    const guardianApiUrl = `https://api.nytimes.com/svc/mostpopular/v2/shared/1.json?api-key=${guardianApiKey}`;
-    const guardianApiData = this.httpService
-      .get(guardianApiUrl)
-      ?.pipe(map((response) => response.data));
-    return guardianApiData;
-  }
   async guardianSearchByWord(wordToSearch: string) {
     const guardianApiKey = '6df96c84-1a3f-4b41-b9eb-22e4300af047';
     const guardianApiUrl = `https://content.guardianapis.com/search?q=${wordToSearch}&api-key=${guardianApiKey}`;
