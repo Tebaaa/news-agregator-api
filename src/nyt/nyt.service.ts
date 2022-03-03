bd245c416d5125a702e6e70a4ef5cf981c909766
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-03 19:49:14 -0300

    Remove commented lines

--- src/nyt/nyt.service.ts
+++ src/nyt/nyt.service.ts
@@ -11,15 +11,6 @@ export class NytService {
     const NYTApiData = this.httpService
       .get(NYTApiUrl)
       ?.pipe(map((response) => response.data.response.docs));
-    // console.log(NYTApiData.subscribe((x) => console.log(x)));
     return NYTApiData;
   }
-  // nytPopularNews() {
-  //   const NYTApiKey = '2bcYsou4gDujh39AZAQ5VsbXzHWcBpZs';
-  //   const NYTApiUrl = `https://api.nytimes.com/svc/mostpopular/v2/shared/1.json?api-key=${NYTApiKey}`;
-  //   const NYTApiData = this.httpService
-  //     .get(NYTApiUrl)
-  //     ?.pipe(map((response) => response.data));
-  //   return NYTApiData;
-  // }
 }
