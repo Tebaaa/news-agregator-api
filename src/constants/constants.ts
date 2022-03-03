46f3401687b5cafccaab460b9b439ba4ca46c739
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-03 20:06:09 -0300

    Export api keys

---
+++ src/constants/constants.ts
@@ -0,0 +1,4 @@
+export const NYTApiKey =
+  process.env.NYT_API_KEY || '2bcYsou4gDujh39AZAQ5VsbXzHWcBpZs';
+export const guardianApiKey =
+  process.env.GUARDIAN_API_KEY || '6df96c84-1a3f-4b41-b9eb-22e4300af047';
