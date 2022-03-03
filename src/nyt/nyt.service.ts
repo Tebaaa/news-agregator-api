d5541a1191b90573f77ede403770456667df7ba2
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-03 20:09:47 -0300

    Await data from httpService

--- src/nyt/nyt.service.ts
+++ src/nyt/nyt.service.ts
@@ -1,17 +1,19 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import { map } from 'rxjs';
+import { lastValueFrom, map } from 'rxjs';
 
 import { NYTApiKey } from 'src/constants/constants';
 
 @Injectable()
 export class NytService {
   constructor(private httpService: HttpService) {}
-  nytSearchByWord(wordToSearch: string) {
+  async nytSearchByWord(wordToSearch: string) {
     const NYTApiUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${wordToSearch}&api-key=${NYTApiKey}`;
-    const NYTApiData = this.httpService
-      .get(NYTApiUrl)
-      ?.pipe(map((response) => response.data.response.docs));
+    const NYTApiData = await lastValueFrom(
+      this.httpService
+        .get(NYTApiUrl)
+        ?.pipe(map((response) => response.data.response.docs)),
+    );
     return NYTApiData;
   }
 }
