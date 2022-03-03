2f4badccb27cd6a24219f81b54eace207c2e5dbd
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-03 20:07:25 -0300

    Import api key from constants file

--- src/guardian/guardian.service.ts
+++ src/guardian/guardian.service.ts
@@ -1,12 +1,12 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { lastValueFrom, map } from 'rxjs';
+import { guardianApiKey } from 'src/constants/constants';
 
 @Injectable()
 export class GuardianService {
   constructor(private httpService: HttpService) {}
   async guardianSearchByWord(wordToSearch: string) {
-    const guardianApiKey = '6df96c84-1a3f-4b41-b9eb-22e4300af047';
     const guardianApiUrl = `https://content.guardianapis.com/search?q=${wordToSearch}&api-key=${guardianApiKey}`;
     const guardianApiData = await lastValueFrom(
       this.httpService
--- src/nyt/nyt.service.ts
+++ src/nyt/nyt.service.ts
@@ -2,11 +2,12 @@ import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { map } from 'rxjs';
 
+import { NYTApiKey } from 'src/constants/constants';
+
 @Injectable()
 export class NytService {
   constructor(private httpService: HttpService) {}
   nytSearchByWord(wordToSearch: string) {
-    const NYTApiKey = '2bcYsou4gDujh39AZAQ5VsbXzHWcBpZs';
     const NYTApiUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${wordToSearch}&api-key=${NYTApiKey}`;
     const NYTApiData = this.httpService
       .get(NYTApiUrl)
