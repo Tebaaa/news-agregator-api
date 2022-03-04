64c3503527221b7cd0d118a44008bb8ab0184961
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-03 22:35:45 -0300

    Interface to handle the Json from guardian API

---
+++ src/guardian/interfaces/guardianNew.interface.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+interface GuardianNew {
+  id: string;
+  type: string;
+  sectionId: string;
+  sectionName: string;
+  webPublicationDate: string;
+  webTitle: string;
+  webUrl: string;
+  apiUrl: string;
+  isHosted: boolean;
+  pillarId: string;
+  pillarName: string;
+}
