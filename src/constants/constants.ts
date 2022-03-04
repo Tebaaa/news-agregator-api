4dad5304d5b4698e49cdd1ef9c3139fa2f2b5f7a
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-03 23:13:36 -0300

    Use port environment variable

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,3 +2,4 @@ export const NYTApiKey =
   process.env.NYT_API_KEY || '2bcYsou4gDujh39AZAQ5VsbXzHWcBpZs';
 export const guardianApiKey =
   process.env.GUARDIAN_API_KEY || '6df96c84-1a3f-4b41-b9eb-22e4300af047';
+export const port = process.env.PORT || 3000;
