1acc83866cc1d9929a87619049e5c1d66bcb6f8c
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-03 22:39:38 -0300

    Return a list with news using the general INews interface

--- src/guardian/guardian.service.ts
+++ src/guardian/guardian.service.ts
@@ -1,6 +1,7 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { lastValueFrom, map } from 'rxjs';
+
 import { guardianApiKey } from 'src/constants/constants';
 
 @Injectable()
@@ -8,11 +9,18 @@ export class GuardianService {
   constructor(private httpService: HttpService) {}
   async guardianSearchByWord(wordToSearch: string) {
     const guardianApiUrl = `https://content.guardianapis.com/search?q=${wordToSearch}&api-key=${guardianApiKey}`;
-    const guardianApiData = await lastValueFrom(
+    const guardianApiData: GuardianNew[] = await lastValueFrom(
       this.httpService
         .get(guardianApiUrl)
         ?.pipe(map((response) => response.data.response.results)),
     );
-    return guardianApiData;
+    const news: INew[] = guardianApiData.map((currentGuardianNew) => {
+      return {
+        title: currentGuardianNew.webTitle,
+        url: currentGuardianNew.webUrl,
+        publication_date: new Date(currentGuardianNew.webPublicationDate),
+      };
+    });
+    return news;
   }
 }
