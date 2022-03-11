1ee4fdbfc56e50220689dbc0a7f1cff8c354692f
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-11 14:25:09 -0300

    Add newsApiOrgKey

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,3 +3,5 @@ export const NYTApiKey =
 export const guardianApiKey =
   process.env.GUARDIAN_API_KEY || '6df96c84-1a3f-4b41-b9eb-22e4300af047';
 export const port = process.env.PORT || 3000;
+export const newsApiOrgKey =
+  process.env.NEWS_API_ORG_KEY || '5949fe99784d4471bbb8da8d768ca687';
