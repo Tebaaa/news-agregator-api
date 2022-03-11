acf3d1b533b06e86502f2385d039f37debe5bac5
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-11 18:01:24 -0300

    Import Users and Auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NewsModule } from './news/news.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [NewsModule],
+  imports: [NewsModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
