359adfc1054492cc0d1040934788fb0167bfcb84
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-11 17:42:06 -0300

    Validate users

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private userService: UsersService) {}
+  async validateUser(userName: string, pass: string) {
+    const user = await this.userService.findOne(userName);
+    if (user && user.password === pass) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+}
