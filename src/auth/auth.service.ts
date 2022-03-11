703ecefd142c39f62ac3303e9340d017827ae3d0
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-11 20:19:50 -0300

    Add login function that returns a token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
   async validateUser(userName: string, pass: string) {
     const user = await this.userService.findOne(userName);
     if (user && user.password === pass) {
@@ -12,5 +16,11 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+  async login(user) {
+    const payload = { username: user.username, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
