aa8be381680b1c1ae700815ca25d2d2a995efcfe
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-11 15:29:08 -0300

    Interface to handle Json from g-News API

---
+++ src/g-news/interfaces/gNew.interface.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+interface GNew {
+  title: string;
+  description: string;
+  content: string;
+  url: string;
+  image: string;
+  publishedAt: Date;
+  source: any;
+}
