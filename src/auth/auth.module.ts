058ecc96be67e55f9e28268af2aaf03aa57591f0
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-12 23:28:04 -0300

    Update import's directories

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,22 +1,23 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
-import { LocalStrategy } from './local.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
 import { AuthController } from './auth.controller';
-import { JwtModule } from '@nestjs/jwt';
 import { jwtCosntants } from './constants';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController],
   imports: [
     UsersModule,
     PassportModule,
     JwtModule.register({
       secret: jwtCosntants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '600s' },
     }),
   ],
 })
