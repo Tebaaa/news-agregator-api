0c4cbe5b56ada84e443b89fc545f35d253f73486
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-12 23:28:55 -0300

    Return the access tokken

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 
-import { LocalAuthGuard } from './local-auth.guard';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './strategies/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 }
