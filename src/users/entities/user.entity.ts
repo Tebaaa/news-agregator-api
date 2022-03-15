fe9974c85cb52aac488474d84fdfc402108fb681
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-15 13:16:37 -0300

    Remove unused import

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,10 +1,4 @@
-import {
-  Column,
-  Entity,
-  JoinColumn,
-  OneToMany,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { SavedNews } from '../saved-news/entities/saved-news.entity';
 
 @Entity()
