3b3570ac50fbbc12ef197e6dcb9b46292fa4849b
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-15 13:08:25 -0300

    Use global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
@@ -5,6 +6,13 @@ import { port } from './constants/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+      transform: true,
+      whitelist: true,
+    }),
+  );
   await app.listen(port);
 }
 bootstrap();
