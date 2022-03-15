1a94380fd2e30ed2c74fa14fc7d54998213d9fc2
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-15 13:05:46 -0300

    Import entities and add providers and controllers

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { SavedNews } from './saved-news/entities/saved-news.entity';
+import { SavedNewsService } from './saved-news/saved-news.service';
 
 @Module({
-  providers: [UsersService],
+  providers: [UsersService, SavedNewsService],
   exports: [UsersService],
+  imports: [TypeOrmModule.forFeature([User, SavedNews])],
+  controllers: [UsersController],
 })
 export class UsersModule {}
