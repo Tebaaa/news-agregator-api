e011402af5a53665ba5d2958b26a1caedb1954b9
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-14 23:59:28 -0300

    Remove unused imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { NewsModule } from './news/news.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [NewsModule, UsersModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    
+    NewsModule, 
+    UsersModule, 
+    AuthModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
