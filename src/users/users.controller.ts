07690f95038169399907bc81f2d4a58441683dbc
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-15 15:36:24 -0300

    Add jwt guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,18 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { SaveNewDto } from './saved-news/dto/save-new.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { SavedNewsService } from './saved-news/saved-news.service';
 import { UsersService } from './users.service';
+import { JwtAuthGuard } from 'src/auth/strategies/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -11,6 +20,7 @@ export class UsersController {
     private userService: UsersService,
     private savedNewsService: SavedNewsService,
   ) {}
+
   @Get()
   getAll() {
     return this.userService.findAll();
@@ -25,14 +35,19 @@ export class UsersController {
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
+
   @Patch(':id')
   update(@Param('id') id: number, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(id, updateUserDto);
   }
+
+  @UseGuards(JwtAuthGuard)
   @Get(':id/news')
-  findAll(@Param('id') id: number) {
+  findAll() {
     return this.savedNewsService.findAll();
   }
+
+  @UseGuards(JwtAuthGuard)
   @Post(':id/news')
   createNew(@Param('id') id: number, @Body() newToSave: SaveNewDto) {
     return this.savedNewsService.create(newToSave, id);
