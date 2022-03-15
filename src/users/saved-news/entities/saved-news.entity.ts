f7b83eafdd525af2654f67605bd259088a9db207
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-15 12:59:19 -0300

    Saved news entity

---
+++ src/users/saved-news/entities/saved-news.entity.ts
@@ -0,0 +1,21 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { User } from '../../entities/user.entity';
+
+@Entity()
+export class SavedNews {
+  @PrimaryGeneratedColumn()
+  relation_id: number;
+  @Column()
+  url_new: string;
+  @Column()
+  user_id: number;
+  @ManyToOne((type) => User, (user) => user.news_saved)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+}
